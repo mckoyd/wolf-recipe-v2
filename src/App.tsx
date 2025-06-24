a11d453fa26c90e9151325cc17625f660ce53d99
Wolf <wolf@Wolfs-MacBook-Pro.local>
2025-06-23 23:24:05 -0400

    RECIPES-11: removes unnecessary redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import { store } from './store/store';
 import Home from './pages/Home';
 import './styles/App.module.scss';
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Provider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 };
 
